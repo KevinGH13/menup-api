2ecdbdc21f5b21c4c0c9cc256355134102fb7312
KevinGH <kevin.garcia@sofka.com.co>
2023-02-09 16:52:17 -0500

    Test case and CI/CD config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TestModule } from './test/test.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    TestModule,
+    MongooseModule.forRoot('mongodb+srv://Kev:root@menupdb.6c0l8.mongodb.net/db_menup?retryWrites=true&w=majority')],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
